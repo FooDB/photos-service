e972e968781178bd37db9500552b9b9f5854ef71
Michael Lee <lee.cs.michael@gmail.com>
2018-07-18 19:58:18 -0700

    Set up development environment

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: __dirname + '/client/src/index.jsx',
+  module: {
+    rules: [
+      { 
+        test: [/\.jsx$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react', 'es2015'],
+        }
+      }
+    ]
+  },
+   output: {
+    filename: 'bundle.js',
+    path: __dirname + '/client/dist'
+  }
+};
+
