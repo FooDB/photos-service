cedc6466d3613b0ff3ea9576d8a9ea8bf72ccb0c
Michael Lee <lee.cs.michael@gmail.com>
2018-08-03 11:44:24 -0700

    Refactor access control

--- server/index.js
+++ server/index.js
@@ -4,10 +4,15 @@ const db = require('../database/retrievePhoto.js');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET');
+  next();
+});
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/restaurant/:id', express.static('public'));
-
 
 app.get('/restaurant/photos/:restaurantId', (req, res) => {
   const id = Number(req.params.restaurantId);
