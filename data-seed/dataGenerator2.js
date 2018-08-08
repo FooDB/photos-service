e504a950fbd4044a75bb8eb0f857c74adc3f7c7a
edwardbaeg <edwardbaeg9@gmail.com>
2018-08-08 10:17:59 -0700

    [bug] cannot generate > 5M records

--- data-seed/dataGenerator2.js
+++ data-seed/dataGenerator2.js
@@ -18,15 +18,33 @@ const getPhotoUrls = () => {
   return photoUrls;
 };
 
+const NUMBER_OF_RECORDS = 10000000;
+
 const genData = () => {
-  for (let i = 0; i < 10; i += 1) {
-    const obj = {};
-    obj.username = `${faker.name.firstName()} ${faker.name.lastName()}`;
-    obj.id = i;
-    obj.description = faker.commerce.productName();
-    obj.createdAt = faker.date.past();
-    obj.photoUrls = getPhotoUrls();
+  let str = `id \t username \t description \t createdAt \t photoUrls \n`;
+  for (let i = 1; i < NUMBER_OF_RECORDS; i += 1) {
+    // let row = 'hi';
+    // let row = `1 \t Winnifred Cremin \t Rustic Steel Chair \t Tue Mar 27 2018 22:33:33 GMT-0700 (PDT) \t ["https://s3-us-west-1.amazonaws.com/fec5-restaurant-photos/food26.jpg","https://s3-us-west-1.amazonaws.com/fec5-restaurant-photos/food40.jpg","https://s3-us-west-1.amazonaws.com/fec5-restaurant-photos/food23.jpg","https://s3-us-west-1.amazonaws.com/fec5-restaurant-photos/food31.jpg","https://s3-us-west-1.amazonaws.com/fec5-restaurant-photos/food21.jpg","https://s3-us-west-1.amazonaws.com/fec5-restaurant-photos/food8.jpg","https://s3-us-west-1.amazonaws.com/fec5-restaurant-photos/food41.jpg","https://s3-us-west-1.amazonaws.com/fec5-restaurant-photos/food12.jpg","https://s3-us-west-1.amazonaws.com/fec5-restaurant-photos/food11.jpg","https://s3-us-west-1.amazonaws.com/fec5-restaurant-photos/food32.jpg","https://s3-us-west-1.amazonaws.com/fec5-restaurant-photos/food39.jpg","https://s3-us-west-1.amazonaws.com/fec5-restaurant-photos/food39.jpg","https://s3-us-west-1.amazonaws.com/fec5-restaurant-photos/food13.jpg","https://s3-us-west-1.amazonaws.com/fec5-restaurant-photos/food37.jpg","https://s3-us-west-1.amazonaws.com/fec5-restaurant-photos/food39.jpg","https://s3-us-west-1.amazonaws.com/fec5-restaurant-photos/food43.jpg","https://s3-us-west-1.amazonaws.com/fec5-restaurant-photos/food22.jpg","https://s3-us-west-1.amazonaws.com/fec5-restaurant-photos/food49.jpg","https://s3-us-west-1.amazonaws.com/fec5-restaurant-photos/food36.jpg","https://s3-us-west-1.amazonaws.com/fec5-restaurant-photos/food21.jpg"]`;
+    let row = '';
+    row += i + '\t';
+    row += `${faker.name.firstName()} ${faker.name.lastName()}` + '\t';
+    // row += faker.commerce.productName() + '\t';
+    // row += faker.date.past() + '\t';
+    // row += JSON.stringify(getPhotoUrls()) + '\n';
+    str += row + '\n';
+    if (i % 1000 === 0) console.log(i);
   }
+  return str;
 };
 
+let fileString = genData();
 
+console.log('done generating data');
+
+fs.writeFile('./database/data.tsv', fileString, (err) => {
+  if (err) {
+    console.log('fs write error', err);
+  } else {
+    console.log('data written to data.tsv');
+  }
+});
