b568abed79dbf01ce6cdfd8f4e93321f460d58b7
edwardbaeg <edwardbaeg9@gmail.com>
2018-08-18 14:53:11 -0700

    Implement redis

--- server/index.js
+++ server/index.js
@@ -2,10 +2,16 @@ require('newrelic');
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const redis = require('redis');
+
 const db = require('../database/index.js');
 
 const app = express();
 app.use(bodyParser.json());
+
+const client = redis.createClient();
+client.on('error', (err) => console.log('Redis error', err));
+client.on('connect', () => console.log('Redis client connected...'));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -20,16 +26,26 @@ app.use('/restaurant/:id', express.static('public'));
 
 app.get('/api/restaurant/:id/photos', (req, res) => {
   const { id } = req.params;
-  db.getById(id)
-    .then((data) => {
-      const ids = data.rows[0].photo_urls;
-      const urls = ids.map(photoId => `https://s3-us-west-1.amazonaws.com/system-design-capstone/${photoId}.jpg`);
-      res.status(200).send(urls);
-    })
-    .catch((error) => {
-      console.log(error);
-      res.status(404).send(error);
-    });
+  // check if data is in redis
+  client.get(id, (error, result) => {
+    if (error) console.log('Redis error', error);
+    if (result) {
+      res.status(200).send(result);
+    } else {
+      db.getById(id)
+        .then((data) => {
+          const ids = data.rows[0].photo_urls;
+          const urls = ids.map(photoId => `https://s3-us-west-1.amazonaws.com/system-design-capstone/${photoId}.jpg`);
+          res.status(200).send(urls);
+          client.set(id, JSON.stringify(urls));
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(404).send(err);
+        });
+    }
+  });
+
 });
 
 app.post('/api/restaurant/:id/addPhoto', (req, res) => {
