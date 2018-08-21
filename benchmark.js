0c80adcc12bfe7b39c15443e5ac18a6dfc905c35
edwardbaeg <edwardbaeg9@gmail.com>
2018-08-20 20:24:51 -0700

    Implement clustering

--- benchmark.js
+++ benchmark.js
@@ -1,10 +1,10 @@
 const siege = require('siege');
 
 const getRandomId = () => {
-  return Math.floor(Math.random() * 1000000) + 9000000;
+  return Math.floor(Math.random() * 100000) + 9900000;
 };
 
-let holder = siege().concurrent(20).on(3002);
+let holder = siege().concurrent(30).on(3002);
 
 for (let i = 0; i < 100000; i += 1) {
   holder.for(1).times.get(`/api/restaurant/${getRandomId()}/photos`);
